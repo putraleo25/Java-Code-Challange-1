6613b7924743581a1442b9947bb25d2208d06583
putrale25 <putrasarwedy25@gmail.com>
2021-10-12 17:25:24 +0700

    first commit

---
+++ BasicJava.java
@@ -0,0 +1,20 @@
+public class BasicJava {
+    public static void main(String[] args) {
+        String firstName, lastName, address, hoby, makanan;
+        long phoneNumber;
+        int age;
+
+        firstName = "Putra";
+        lastName = "Leo";
+        address = "Batu Aji";
+        hoby = "Travelling";
+        makanan = "Singkong";
+        phoneNumber = +628218827;
+        age = 23;
+
+        System.out.println("Nama              : " + firstName + lastName + "\n" + "Alamat             : " + address
+                + "\n" + "Age                : " + age + "Tahun" + "\n" + "hobi               : " + hoby + "\n"
+                + "Telepon            : " + phoneNumber + "\n" + "Makanan Kesukaan   : " + makanan + "\n\n"
+                + "Selesai");
+    }
+}
